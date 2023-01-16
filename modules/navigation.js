9a2aa12af65d1a0511d26b0e19eb3761536983d9
Usama Masood <umasood.bee17seecs@seecs.edu.pk>
2023-01-16 14:42:01 +0500

    Addition of arrow syntax of functions

--- modules/navigation.js
+++ modules/navigation.js
@@ -2,20 +2,20 @@ const listSection = document.querySelector('#list');
 const addNewSection = document.querySelector('#add-new');
 const contactSection = document.querySelector('#contact');
 
-export function showListSection() {
+export const showListSection = () => {
   listSection.style.display = 'block';
   addNewSection.style.display = 'none';
   contactSection.style.display = 'none';
-}
+};
 
-export function showAddNew() {
+export const showAddNew = () => {
   addNewSection.style.display = 'block';
   listSection.style.display = 'none';
   contactSection.style.display = 'none';
-}
+};
 
-export function showContact() {
+export const showContact = () => {
   contactSection.style.display = 'block';
   addNewSection.style.display = 'none';
   listSection.style.display = 'none';
-}
+};
